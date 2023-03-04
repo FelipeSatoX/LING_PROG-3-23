6702f047cf4c1bdede4747428b55a948e15f0b8f
Felipe Satox <125518285+FelipeSatoX@users.noreply.github.com>
2023-03-03 21:15:14 -0300

    Add files via upload

---
+++ LING_PROG-3-23-main/LING_PROG-3-23-main/1_intro/1_comentário.php
@@ -0,0 +1,12 @@
+<?php
+ 
+   echo "isso é um comentário";
+   // comentário 1
+   
+   /* comentário 2
+       em duas linhas
+   */
+   
+   #comentário 3
+
+?>
--- LING_PROG-3-23-main/LING_PROG-3-23-main/1_intro/2_variaveis.php
+++ LING_PROG-3-23-main/LING_PROG-3-23-main/1_intro/2_variaveis.php
@@ -1,19 +1,20 @@
 <?php
-    $name = "denicreidsson"
+    $name = "Denicreidsson";
 	$age = 24;
 	echo $name;
 	echo "<br>";
 	echo $age;
+	
 	echo "<hr>";
 	$b = true;
 	echo gettype($b);
 	
-	echo "<hr>";	
+	echo "<hr>";
 	$nasc = 1994;
 	echo gettype($nasc);
 	
-	echo "<hr>";	
-	$float = "99.90";
+	echo "<hr>";
+	$float = 99.90;
 	echo gettype($float);
 	
 ?>
